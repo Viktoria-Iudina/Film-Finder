be0ab49356fc0151f594bc637019f132eb7084fb
Viktoria-Iudina <jajudinaja@gmail.com>
2024-01-09 16:08:27 +0100

    Getting a list of movies and displaying a random movie from the list

--- public/script.js
+++ public/script.js
@@ -1,2 +1,13 @@
 const tmdbKey = 'f4c49a2483b5408d5619cdb6a90e5867';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+
+
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+};
